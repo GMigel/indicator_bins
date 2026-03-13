c48e20af8488214c35a948669b6747667e064a19
Голубятников Михаил Борисович <gmb@spsinf.ru>
2026-03-13 13:28:49 +0300

    Builded PC & STM32

---
+++ app/app_pc.cpp
@@ -0,0 +1,20 @@
+#include "app.h"
+#include "../gui/gui.h"
+
+#include <math.h>
+// #include <cstdio>
+#include <array>
+
+#include "air_signals.h"
+#include "build_time.h"
+
+#include <cstdint>
+#include <cstdio>
+
+void app_run() {
+};
+
+void app_on_timer() {
+};
+
+
