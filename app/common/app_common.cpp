50ccb715aa7d6c6ba2f35513e64b191f10a1ddeb
Голубятников Михаил Борисович <gmb@spsinf.ru>
2026-03-15 16:54:16 +0300

    PC sim works without mouse

--- app/common/app_common.cpp
+++ app/common/app_common.cpp
@@ -1,71 +1,68 @@
 #include "app.h"
 #include "app_cpp.h"
 
-#include <math.h>
-#include <cstdio>
-#include <stdint.h>
 #include <array>
+#include <cstdio>
+#include <math.h>
+#include <stdint.h>
 
+#include "RotaryEncoder.h"
 #include "air_signals.h"
 #include "build_time.h"
-#include "RotaryEncoder.h"
-#include "gui.h"
-#include "disp_bright.h"
-#include "input_hal.h"
 #include "can_messages.h"
+#include "disp_bright.h"
+#include "gui.h"
+#include "input_hal.h"
 #include "rs422_port.h"
 
-void app_on_timer()
-{
-    systime = systime + 1;
+static uint32_t test_timeout = 0;
 
-    if (gui_state.mode == GUI_INIT)
-    {
-        if (systime == 500)
-            gui_state.mode = GUI_TEST;
+void app_on_timer() {
+  systime = systime + 1;
+  printf("mode %d timeout %d\n", gui_state.mode, test_timeout);
+
+  switch (gui_state.mode) {
+  case GUI_INIT:
+    if (systime >= 500) {
+      gui_state.mode = GUI_TEST;
+      test_timeout = 1000;
     }
-    else if (gui_state.mode == GUI_TEST)
-    {
-        static uint32_t test_timeout = 1000;
-        if (test_timeout == 0)
-        {
-            gui_state.mode = GUI_MAIN_MODE;
-            test_timeout = 1000;
-        }
-        else
-            test_timeout--;
+    break;
+  case GUI_TEST:
+    if (test_timeout <= 0) {
+      gui_state.mode = GUI_MAIN_MODE;
+    } else {
+      test_timeout--;
     }
+    break;
+  default:
+    break;
+  }
 
-    input_poll();
+  input_poll();
 
-    rs422_serve();
- //	lv_tick_inc(HAL_TICK_FREQ_DEFAULT); //ToDo
-     lv_tick_inc(1); // LVGL ожидает миллисекунды.  //ToDo
- //    lv_tick_inc(10); // если таймер 10 ms: //ToDo
- 	encoder.serve_input(input_get_enc(), input_get_btn());
+  rs422_serve();
+  //	lv_tick_inc(HAL_TICK_FREQ_DEFAULT); //ToDo
+  lv_tick_inc(1); // LVGL ожидает миллисекунды.  //ToDo
+                  //    lv_tick_inc(10); // если таймер 10 ms: //ToDo
+  encoder.serve_input(input_get_enc(), input_get_btn());
 
-    static uint32_t cntr = 0;
-    if (++cntr == 1000)
-    {
-        cntr = 0;
+  static uint32_t cntr = 0;
+  if (++cntr == 1000) {
+    cntr = 0;
 
-        // Версия для CAN
-static const can_msg_version_t version =
-{
-    .timestamp = BuildTime,
-    .maj_ver   = MajVersion,
-    .min_ver   = MinVersion,
-    .build     = Build
-};
+    // Версия для CAN
+    static const can_msg_version_t version = {
+        .timestamp = BuildTime, .maj_ver = MajVersion, .min_ver = MinVersion, .build = Build};
 
-can_send_dat(CAN_VERSION_MFI, &version, sizeof(version));
-    }
+    can_send_dat(CAN_VERSION_MFI, &version, sizeof(version));
+  }
 }
 
 #if APP_DEBUG
-    #define debug_print rs422_printf
+#define debug_print rs422_printf
 #else
-    #define debug_print(...)
+#define debug_print(...)
 #endif
 
 using namespace std;
@@ -86,258 +83,235 @@ RotaryEncoder::listener_t enc_menu = {menu_rotate, menu_click, menu_hold, menu_t
 RotaryEncoder::listener_t enc_menu_timeless = {menu_rotate, menu_click, menu_hold, dummy_timeout};
 
 #if USE_MTI_ICC
-const char* main_menu_items[] = {"ЕД. ИЗМ. ДАВЛЕНИЯ","ЕД. ИЗМ. ВЫСОТЫ", "КАЛИБРОВКА КОМПАСА", "ВЕРСИЯ"};
+const char *main_menu_items[] = {"ЕД. ИЗМ. ДАВЛЕНИЯ", "ЕД. ИЗМ. ВЫСОТЫ", "КАЛИБРОВКА КОМПАСА",
+                                 "ВЕРСИЯ"};
 gui_menu_t menu_main = {nullptr, main_menu_items, 4, 0, menu_select};
 #else
-const char* main_menu_items[] = {"ЕД. ИЗМ. ДАВЛЕНИЯ","ЕД. ИЗМ. ВЫСОТЫ", "ВЕРСИЯ"};
+const char *main_menu_items[] = {"ЕД. ИЗМ. ДАВЛЕНИЯ", "ЕД. ИЗМ. ВЫСОТЫ", "ВЕРСИЯ"};
 gui_menu_t menu_main = {nullptr, main_menu_items, 3, 0, menu_select};
 #endif
 
-const char* pres_items[] = {"мм. рт. ст","гПа"};
+const char *pres_items[] = {"мм. рт. ст", "гПа"};
 gui_menu_t menu_pres = {"ЕДИНИЦЫ ДАВЛЕНИЯ", pres_items, 2, 0, pres_select};
 
-const char* alt_items[] = {"метры","футы"};
+const char *alt_items[] = {"метры", "футы"};
 gui_menu_t menu_alt = {"ЕДИНИЦЫ ВЫСОТЫ", alt_items, 2, 0, alt_select};
 
-const char* ver_items[] = {"ПО БИНС: -","ПО МФИ: 0.5", "ЗАГРУЗЧИК БИНС: -", "ЗАГРУЗЧИК МФИ: -"};
+const char *ver_items[] = {"ПО БИНС: -", "ПО МФИ: 0.5", "ЗАГРУЗЧИК БИНС: -", "ЗАГРУЗЧИК МФИ: -"};
 gui_menu_t menu_ver = {"ВЕРСИЯ", ver_items, 4, 0, ver_select};
 
 #if USE_MTI_ICC
-const char* icc_items[] = {"вкл","выкл"};
+const char *icc_items[] = {"вкл", "выкл"};
 gui_menu_t menu_icc = {"КАЛИБРОВКА КОМПАСА", icc_items, 2, 0, icc_select};
 
-const char* bad_icc_items[] = {"ОК"};
-gui_menu_t menu_bad_icc = {"НЕДОСТАТОЧНО ДАННЫХ\rДЛЯ КАЛИБРОВКИ", bad_icc_items, 1, 0, bad_icc_select};
+const char *bad_icc_items[] = {"ОК"};
+gui_menu_t menu_bad_icc = {"НЕДОСТАТОЧНО ДАННЫХ\rДЛЯ КАЛИБРОВКИ", bad_icc_items, 1, 0,
+                           bad_icc_select};
 
-const char* good_icc_items[] = {"СОХРАНИТЬ","ОТМЕНА"};
+const char *good_icc_items[] = {"СОХРАНИТЬ", "ОТМЕНА"};
 gui_menu_t menu_good_icc = {nullptr, good_icc_items, 2, 0, good_icc_select};
 #endif
 
-gui_state_t gui_state = {0.0f, };
+gui_state_t gui_state = {
+    // 0.0f,
+    0,
+};
 RotaryEncoder encoder(enc_brg);
 volatile uint32_t systime = 0;
 
 //------------------------------------------------------------------------------
-void brg_rotate(int8_t delta)
-{
-	int brg = disp_bright_get();
-	brg += (1+brg/10)*delta;
-	if(brg < 0)
-		brg = 0;
-	else if(brg > 255)
-		brg = 255;
-	disp_bright_set((uint8_t)brg);
-	debug_print("brightness %d\r", brg);
+void brg_rotate(int8_t delta) {
+  int brg = disp_bright_get();
+  brg += (1 + brg / 10) * delta;
+  if (brg < 0)
+    brg = 0;
+  else if (brg > 255)
+    brg = 255;
+  disp_bright_set((uint8_t)brg);
+  debug_print("brightness %d\r", brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void brg_click()
-{
-	gui_state.mode = GUI_SET_PRES_REF;
-	encoder.change_listener(enc_ref);
+void brg_click() {
+  gui_state.mode = GUI_SET_PRES_REF;
+  encoder.change_listener(enc_ref);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void brg_hold()
-{
-	gui_state.menu = &menu_main;
-	gui_state.mode = GUI_MENU;
-	encoder.change_listener(enc_menu);
+void brg_hold() {
+  gui_state.menu = &menu_main;
+  gui_state.mode = GUI_MENU;
+  encoder.change_listener(enc_menu);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void ref_rotate(int8_t delta)
-{
-	int type_nr = (int)gui_state.pres_type;
-	if(delta>0)
-		gui_state.pres_type = (gui_type_pres_t)((type_nr < 2) ? ++type_nr : 0);
-	else if(delta<0)
-		gui_state.pres_type = (gui_type_pres_t)((type_nr > 0) ? --type_nr : 2);
+void ref_rotate(int8_t delta) {
+  int type_nr = (int)gui_state.pres_type;
+  if (delta > 0)
+    gui_state.pres_type = (gui_type_pres_t)((type_nr < 2) ? ++type_nr : 0);
+  else if (delta < 0)
+    gui_state.pres_type = (gui_type_pres_t)((type_nr > 0) ? --type_nr : 2);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void ref_click()
-{
-	if(gui_state.pres_type == GUI_QNE)
-	{
-		gui_state.mode = GUI_MAIN_MODE;
-		encoder.change_listener(enc_brg);
-	}
-	else
-	{
-		gui_state.mode = GUI_SET_PRES_VAL;
-		encoder.change_listener(enc_pres);
-	}
+void ref_click() {
+  if (gui_state.pres_type == GUI_QNE) {
+    gui_state.mode = GUI_MAIN_MODE;
+    encoder.change_listener(enc_brg);
+  } else {
+    gui_state.mode = GUI_SET_PRES_VAL;
+    encoder.change_listener(enc_pres);
+  }
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void pres_rotate(int8_t delta)
-{
-	if(gui_state.pres_type == GUI_QFE)
-		gui_state.pres_qfe += delta*10;
-	else if(gui_state.pres_type == GUI_QNH)
-		gui_state.pres_qnh += delta*10;
+void pres_rotate(int8_t delta) {
+  if (gui_state.pres_type == GUI_QFE)
+    gui_state.pres_qfe += delta * 10;
+  else if (gui_state.pres_type == GUI_QNH)
+    gui_state.pres_qnh += delta * 10;
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void pres_click()
-{
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void pres_click() {
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void menu_rotate(int8_t delta)
-{
-	int item_nr = (int)gui_state.menu->curr_item;
-	if(delta>0)
-		gui_state.menu->curr_item = ((item_nr < (gui_state.menu->items_num-1)) ? ++item_nr : 0);
-	else if(delta<0)
-		gui_state.menu->curr_item = ((item_nr > 0) ? --item_nr : (gui_state.menu->items_num-1));
+void menu_rotate(int8_t delta) {
+  int item_nr = (int)gui_state.menu->curr_item;
+  if (delta > 0)
+    gui_state.menu->curr_item = ((item_nr < (gui_state.menu->items_num - 1)) ? ++item_nr : 0);
+  else if (delta < 0)
+    gui_state.menu->curr_item = ((item_nr > 0) ? --item_nr : (gui_state.menu->items_num - 1));
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void menu_click()
-{
-	gui_state.menu->on_select(gui_state.menu->curr_item);
+void menu_click() { gui_state.menu->on_select(gui_state.menu->curr_item); }
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+void menu_hold() {
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void menu_hold()
-{
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void menu_timeout() {
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void menu_timeout()
-{
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
-}
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-void menu_select(uint8_t item)
-{
-	switch(item)
-	{
-	case 0:
-		gui_state.menu = &menu_pres;
-		break;
-	case 1:
-		gui_state.menu = &menu_alt;
-		break;
+void menu_select(uint8_t item) {
+  switch (item) {
+  case 0:
+    gui_state.menu = &menu_pres;
+    break;
+  case 1:
+    gui_state.menu = &menu_alt;
+    break;
 #if USE_MTI_ICC
-	case 2:
-		gui_state.menu = &menu_icc;
-		break;
-	case 3:
-		gui_state.menu = &menu_ver;
-		break;
+  case 2:
+    gui_state.menu = &menu_icc;
+    break;
+  case 3:
+    gui_state.menu = &menu_ver;
+    break;
 #else
-	case 2:
-	  gui_state.menu = &menu_ver;
-	  break;
+  case 2:
+    gui_state.menu = &menu_ver;
+    break;
 #endif
-	}
+  }
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void pres_select(uint8_t item)
-{
-	switch(item)
-	{
-	case 0:
-		gui_state.pres_unit = GUI_MERC;
-		break;
-	case 1:
-		gui_state.pres_unit = GUI_PASC;
-		break;
-	}
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void pres_select(uint8_t item) {
+  switch (item) {
+  case 0:
+    gui_state.pres_unit = GUI_MERC;
+    break;
+  case 1:
+    gui_state.pres_unit = GUI_PASC;
+    break;
+  }
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void alt_select(uint8_t item)
-{
-	switch(item)
-	{
-	case 0:
-		gui_state.alt_unit = GUI_METER;
-		break;
-	case 1:
-		gui_state.alt_unit = GUI_FOOT;
-		break;
-	}
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void alt_select(uint8_t item) {
+  switch (item) {
+  case 0:
+    gui_state.alt_unit = GUI_METER;
+    break;
+  case 1:
+    gui_state.alt_unit = GUI_FOOT;
+    break;
+  }
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-void ver_select(uint8_t item)
-{
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void ver_select(uint8_t item) {
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 #if USE_MTI_ICC
-void icc_select(uint8_t item)
-{
-	can_icc_cmd_t cmd;
-	switch(item)
-	{
-	case 0:
-		cmd = 0x0;
-		break;
-	case 1:
-		cmd = 0x1;
-		break;
-	}
-	can_send_dat(CAN_CMD_ICC, &cmd, sizeof(cmd));
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void icc_select(uint8_t item) {
+  can_icc_cmd_t cmd;
+  switch (item) {
+  case 0:
+    cmd = 0x0;
+    break;
+  case 1:
+    cmd = 0x1;
+    break;
+  }
+  can_send_dat(CAN_CMD_ICC, &cmd, sizeof(cmd));
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 #endif
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 #if USE_MTI_ICC
-void bad_icc_select(uint8_t item)
-{
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void bad_icc_select(uint8_t item) {
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 #endif
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 #if USE_MTI_ICC
-void good_icc_select(uint8_t item)
-{
-	if(item == 0)
-	{
-		can_icc_cmd_t cmd = CAN_CMD_ICC_SRORE;
-		can_send_dat(CAN_CMD_ICC, &cmd, sizeof(cmd));
-	}
-	gui_state.mode = GUI_MAIN_MODE;
-	encoder.change_listener(enc_brg);
+void good_icc_select(uint8_t item) {
+  if (item == 0) {
+    can_icc_cmd_t cmd = CAN_CMD_ICC_SRORE;
+    can_send_dat(CAN_CMD_ICC, &cmd, sizeof(cmd));
+  }
+  gui_state.mode = GUI_MAIN_MODE;
+  encoder.change_listener(enc_brg);
 }
 #endif
 //------------------------------------------------------------------------------
--- app/pc/app_pc_stubs.cpp
+++ app/pc/app_pc_stubs.cpp
@@ -2,9 +2,9 @@
 
 #define HAL_TICK_FREQ_DEFAULT 5
 
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
-#include <cmath>
 
 #include "app.h"
 #include "gui.h"
@@ -16,110 +16,92 @@ extern volatile uint32_t systime;
 
 // ---------------- CAN simulation ----------------
 
-void can_init()
-{
-    printf("[PC] CAN init (simulated)\n");
-}
+void can_init() { printf("[PC] CAN init (simulated)\n"); }
 
-void rs422_serve()
-{
-}
+void rs422_serve() {}
 
 /*
  * Generate smooth aircraft motion for GUI simulation
  */
-void can_serve()
-{
-    float t = systime * 0.001f;
+void can_serve() {
+  float t = systime * 0.001f;
 
-    gui_state.valid_roll = true;
-    gui_state.roll = sinf(t * 0.7f) * 0.6f;
+  gui_state.valid_roll = true;
+  gui_state.roll = sinf(t * 0.7f) * 0.6f;
 
-    gui_state.valid_pitch = true;
-    gui_state.pitch = sinf(t * 0.5f) * 0.3f;
+  gui_state.valid_pitch = true;
+  gui_state.pitch = sinf(t * 0.5f) * 0.3f;
 
-    gui_state.valid_heading = true;
-    gui_state.heading = fmodf(t * 0.3f, 6.2831853f);
+  gui_state.valid_heading = true;
+  gui_state.heading = fmodf(t * 0.3f, 6.2831853f);
 
-    gui_state.valid_altitude = true;
-    gui_state.altitude = 1200.0f + sinf(t * 0.1f) * 200.0f;
+  gui_state.valid_altitude = true;
+  gui_state.altitude = 1200.0f + sinf(t * 0.1f) * 200.0f;
 
-    gui_state.valid_speed = true;
-    gui_state.speed = 220.0f + sinf(t * 0.4f) * 30.0f;
+  gui_state.valid_speed = true;
+  gui_state.speed = 220.0f + sinf(t * 0.4f) * 30.0f;
 
-    gui_state.valid_ver_speed = true;
-    gui_state.vert_speed = sinf(t * 0.6f) * 8.0f;
+  gui_state.valid_ver_speed = true;
+  gui_state.vert_speed = sinf(t * 0.6f) * 8.0f;
 }
 
-bool can_send_dat(uint16_t id, const void* data, const uint8_t len)
+bool can_send_dat(uint16_t id, const void *data, const uint8_t len)
 // void can_send_dat(int id, const void* data, size_t len)
 {
-    (void)data;
+  (void)data;
 
-    printf("[PC] CAN TX id=%d len=%zu\n", id, len);
+  // printf("[PC] CAN TX id=%d len=%zu\n", id, len);
 
-    return false;
+#ifndef PLATFORM_PC
+  printf("[PC] CAN TX id=%d len=%zu\n", id, len);
+#else
+  printf("[PC] CAN TX id=%d len=%u\n", id, len);
+#endif
+
+  return false;
 }
 
 // ---------------- Display brightness ----------------
 
 static uint8_t bright = 128;
 
-void disp_bright_init()
-{
-    printf("[PC] Display brightness init\n");
-}
+void disp_bright_init() { printf("[PC] Display brightness init\n"); }
 
-void disp_bright_set(uint8_t val)
-{
-    bright = val;
-}
+void disp_bright_set(uint8_t val) { bright = val; }
 
-uint8_t disp_bright_get()
-{
-    return bright;
-}
+uint8_t disp_bright_get() { return bright; }
 
 // ---------------- Encoder / button simulation ----------------
 
 static int8_t enc_val = 0;
 static bool btn = false;
 
-void input_init()
-{
-    printf("[PC] Input init\n");
+void input_init() { printf("[PC] Input init\n"); }
+
+void input_poll() {
+  /*
+   * Automatic encoder demo
+   * Slowly rotates encoder to demonstrate menus
+   */
+
+  static uint32_t last = 0;
+
+  if (systime - last > 200) {
+    enc_val++;
+    last = systime;
+  }
+
+  if (enc_val > 10)
+    enc_val = -10;
 }
 
-void input_poll()
-{
-    /*
-     * Automatic encoder demo
-     * Slowly rotates encoder to demonstrate menus
-     */
-
-    static uint32_t last = 0;
-
-    if(systime - last > 200)
-    {
-        enc_val++;
-        last = systime;
-    }
-
-    if(enc_val > 10)
-        enc_val = -10;
+int8_t input_get_enc() {
+  int8_t v = enc_val;
+  enc_val = 0;
+  return v;
 }
 
-int8_t input_get_enc()
-{
-    int8_t v = enc_val;
-    enc_val = 0;
-    return v;
-}
-
-bool input_get_btn()
-{
-    return btn;
-}
+bool input_get_btn() { return btn; }
 
 } // extern "C"
 
