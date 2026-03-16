efc0fb40a24bf04f5b95cf5b208322b90de819a6
Голубятников Михаил Борисович <gmb@spsinf.ru>
2026-03-16 17:17:19 +0300

    STM and PC builded OK

--- app/stm32/disp_bright_hal.c
+++ app/stm32/disp_bright_hal.c
@@ -1,17 +1,10 @@
 #include "disp_bright.h"
 #include "tim.h"
 
-void disp_bright_init(void)
-{
-    HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_1); // enable display backlight
+void disp_bright_init(void) {
+  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_1); // enable display backlight
 }
 
-void disp_bright_set(uint8_t brg)
-{
-    __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, brg);
-}
+void disp_bright_set(uint8_t brg) { __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, brg); }
 
-uint8_t disp_bright_get(void)
-{
-    return __HAL_TIM_GET_COMPARE(&htim12, TIM_CHANNEL_1);
-}
+uint8_t disp_bright_get(void) { return __HAL_TIM_GET_COMPARE(&htim12, TIM_CHANNEL_1); }
